8d9028c4a1cf189d42aab97af818dcf1b8d817b7
lazyproof1 <lao_steven@hotmail.com>
2015-01-27 17:49:14 -0500

    ResourceTest progress

--- src/ca/etsmtl/log430/lab1/tests/ResourceTest.java
+++ src/ca/etsmtl/log430/lab1/tests/ResourceTest.java
@@ -4,36 +4,58 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import ca.etsmtl.log430.lab1.donnees.Project;
+import ca.etsmtl.log430.lab1.donnees.Resource;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Random;
+
 public class ResourceTest {
 
+	Resource testRes = new Resource();
+	Project testProject = new Project();
+	
 	@Test
-	public void testAssignProject() {
-		fail("Not yet implemented");
+	public void testAssignProject() throws Exception {
+
+		// Create a new project
+		
+		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+		Date startDate1 = formatter.parse(testProject.getStartDate());
+		Date endDate1 = formatter.parse(testProject.getEndDate());
+		
+		
+		
 	}
 
 	@Test
 	public void testMapPriority() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testSetPreviouslyAssignedProjectList() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetPreviouslyAssignedProjectList() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testSetProjectsAssigned() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetProjectsAssigned() {
-		fail("Not yet implemented");
+		
+		// Verify if method return the corresponding value
+		assertEquals(Resource.HIGH, testRes.mapPriority('H'));
+		assertEquals(Resource.MED, 	testRes.mapPriority('M'));
+		assertEquals(Resource.LOW,	testRes.mapPriority('L'));
+		
+		assertNotEquals(Resource.LOW,	testRes.mapPriority('M'));
+		assertNotEquals(Resource.LOW,	testRes.mapPriority('H'));
+		assertNotEquals(Resource.MED,	testRes.mapPriority('L'));
+		assertNotEquals(Resource.MED,	testRes.mapPriority('H'));
+		assertNotEquals(Resource.HIGH,	testRes.mapPriority('L'));
+		assertNotEquals(Resource.HIGH,	testRes.mapPriority('M'));
+		
+		Random r = new Random();
+		char c;
+		
+		// Verify unidentified letters
+		for(int i = 0; i < 100; i++){
+			c = (char)(r.nextInt(26) + 'A');
+			if (c != 'H' && c != 'M' && c != 'L'){
+				assertEquals(0, testRes.mapPriority(c));
+			} // if
+		} // for
+		
 	}
 
 }
